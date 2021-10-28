e76e87ec00f4f7ccd0f24070e3e1dd70c98889e4
NiteshMankar1 <93311533+NiteshMankar1@users.noreply.github.com>
2021-10-28 19:57:37 +0530

    Update HealthyLife.java

--- src/HealthyLife/HealthyLife.java
+++ src/HealthyLife/HealthyLife.java
@@ -1,39 +1,39 @@
-package HealthyLife;                                                // Name of package 
+package HealthyLife;                                                                         // Name of package 
 
-import java.io.BufferedReader;                                      // To Read character data
-import java.io.InputStreamReader;                                   // To Read bytes and convert them in characters
-import java.net.URL;                                                // To Access the URL
-import java.net.URLEncoder;                                         // To allow spaces because space are not allowed in URL
-import java.time.LocalDate;                                         // Specifying Date
-import java.time.LocalTime;                                         // Specifying Time
+import java.io.BufferedReader;                                                               // To Read character data
+import java.io.InputStreamReader;                                                            // To Read bytes and convert them in characters
+import java.net.URL;                                                                         // To Access the URL
+import java.net.URLEncoder;                                                                  // To allow spaces because space are not allowed in URL
+import java.time.LocalDate;                                                                  // Specifying Date
+import java.time.LocalTime;                                                                  // Specifying Time
 import java.util.*;
 
-import javax.net.ssl.HttpsURLConnection;                            // Security to secure connection between web server and web browser
+import javax.net.ssl.HttpsURLConnection;                                                     // Security to secure connection between web server and web browser
  public class HealthyLife
  {
-	 public static void sendSms(String message,String number)       // Method sendSms with parameters
+	 public static void sendSms(String message,String number)                            // Method sendSms with parameters
 	 
 	 
 	 {
 		 System.out.println(message);
 		 System.out.println(number);
-		 try                                                        // We are using this all in try and catch block to avoid mistakes 
+		 try                                                                         // We are using this all in try and catch block to avoid mistakes 
 		 {
 			 String apikey="83sf6iHjpQCPDqARu5lTdSXmkFn74eUOrgGxazLc0YVIKtvbJoiLDu64KQlt5m9dvCGUkjWR8z3ZNbTI";     // Saving apikey taken from message gateway company
-			 String sendID="FSTSMS";                                // Specifying SenderID
-			                                                                                               //important step               
-			 message=URLEncoder.encode(message, "UTF-8");           // To decode characters into Unicode (Unicode= Each char,no,symbol has its own number) 
+			 String sendID="FSTSMS";                                             // Specifying SenderID
+			                                                                     // important step               
+			 message=URLEncoder.encode(message, "UTF-8");                        // To decode characters into Unicode (Unicode= Each char,no,symbol has its own number) 
 			 
-			 String language="english";                             // Specifying Language given on the message gateway platform
-			 
+			 String language="english";                                          // Specifying Language given on the message gateway platform
+			  
 			 String route="p";                                       
 			 
 			 
 			 String myUrl="https://www.fast2sms.com/dev/bulkV2?authorization="+apikey+"&sender_id="+sendID+"&message="+message+"&variable_values="+language+"&route="+route+"&numbers="+number;
 			
-			                                                        // Sending get request to get message  
+			                                                                     // Sending get request to get message  
 			 
-			 URL url=new URL(myUrl);                                // Creation of object of myURL
+			 URL url=new URL(myUrl);                                             // Creation of object of myURL
 			 
 			 HttpsURLConnection con=(HttpsURLConnection)url.openConnection();   //  Opens Connection with specified URL   
 			 
